706c72bb944b0aa1f30b4ab037688d257d215a5f
Joshua Coffey <geel@scrap.tf>
2020-07-14 06:25:38 -0400

    Actual initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Example
-An example greeter plugin
+#H.A.M Store Rooms
+This is a simple plugin which highlights the chests in the H.A.M Store Rooms when you have the required key to unlock them.
+
+It also displays *how many times* you can open said chest.
--- src/test/java/com/geel/hamstorerooms/HAMStoreRoomsPluginTest.java
+++ src/test/java/com/geel/hamstorerooms/HAMStoreRoomsPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.geel.hamstorerooms;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class HAMStoreRoomsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(StoreRoomsPlugin.class);
 		RuneLite.main(args);
 	}
 }
